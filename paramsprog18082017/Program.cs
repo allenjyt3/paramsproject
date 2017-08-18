dfd32c5642035fbdc829e70a4a9ba12a4751663c
ajasseessi <ajasseessi@SUYPC214>
2017-08-18 18:05:18 +0530

    Add project files.

---
+++ paramsprog18082017/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramsprog18082017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample sample = new Sample();
+
+            sample.add(1, 12, 13,"hi");
+
+            //sample.add(1, 12, 13);
+            Console.ReadLine();
+
+        }
+
+    }
+
+   public class Sample
+    {
+        //public void add(params int[] arr)
+        public void add(params object[] arr)
+
+        {
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum =sum +  arr[i];
+            //}
+            // Console.Write(sum);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+    }
+
+
+
+
+
+
+}
+
+
+
